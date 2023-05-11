72d9c9478f8bece49b4e3b7611156431459b7ff0
ramonvilelaIDE <ramonvilela92@hotmail.com>
2023-05-11 17:48:14 -0300

    aula sintaxe

---
+++ java-documentation/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    /*Tipos diferentes de comentários */
+    
+    // Olá, eu sou um comentário em uma única linha (One line)
+
+
+/* (mult line) 
+ *Olá,
+ * Eu sou um comentario
+ * que posso ser mais detalhado
+ * quando necessário
+ */
+
+ /** (Documentation)
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+}
