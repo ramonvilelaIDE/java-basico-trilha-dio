804f576e4ea4ad3a74df8afa5305b2c23357c9f1
ramonvilelaIDE <ramonvilela92@hotmail.com>
2023-05-10 21:01:39 -0300

    operadores feito

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos: os mais comuns são int e double.
+        //string 
+
+        String meuNome = "Ramon Vilela";
+        
+        double salarioMinimo = 2500;
+
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //nessa linha usou-se o recurso cast que é transformar um tipo mais abrangente num mais específico (int em short)
+
+      
     }
 }
