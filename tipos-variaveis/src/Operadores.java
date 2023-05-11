dfaef2ee6a5faadf460b8f7cab5c34880ef9e69b
ramonvilelaIDE <ramonvilela92@hotmail.com>
2023-05-11 13:12:30 -0300

    operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,6 +3,18 @@ public class Operadores {
        /*  String nomeCompleto = "LINGUAGEM" + "JAVA";//esse '+' concatena as duas palavras.
         
         System.out.println(nomeCompleto); */
-        
+      
+        // ComparacaoAvancada.java
+int numero1 = 1;
+int numero2 = 1;
+
+if(numero1== 2 & numero2 ++ == 2 )
+    System.out.println("As 2 condições são verdadeiras");
+
+System.out.println("O numero 1 agora é " + numero1);
+System.out.println("O numero 2 agora é " + numero2);
+
+// Vamos mudar a linha 5 do código acima para: if(numero1== 2 && numero2 ++ == 2 )
+
     }
 }
